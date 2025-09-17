04c492cd2aace5eba8cd24fa18ed1eea0d3d0b2d
xiaoR <2471385107@qq.com>
2025-09-17 15:42:50 +0800

    refactor(hm-api): 重构购物车服务并添加公共 API 模块
    - 从 cart-service 中提取 ItemClient 和 ItemDTO 到新的 hm-api 模块
    - 更新 cart-service 依赖，添加对 hm-api 的引用
    - 修改数据库连接字符串，添加 allowPublicKeyRetrieval 参数
    - 优化 CartApplication 启动类，指定 Feign 客户端包, 将各服务对外暴露的接口进行统一管理

--- cart-service/src/main/java/com/hmall/cart/CartApplication.java
+++ cart-service/src/main/java/com/hmall/cart/CartApplication.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @MapperScan("com.hmall.cart.mapper")
-@EnableFeignClients     // 启动OpenFeign功能
+@EnableFeignClients(basePackages = "com.hmall.api.client")    // 启动OpenFeign功能
 @SpringBootApplication
 public class CartApplication {
     public static void main(String[] args) {
