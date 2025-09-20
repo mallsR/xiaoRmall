4cc61dbd02a9192c7a32bbcde998ecbbe67d1c63
xiaoR <2471385107@qq.com>
2025-09-20 11:00:33 +0800

    feat(gateway): 初始化网关服务配置和启动类
    - 新增网关服务的基础配置文件 application.yaml，配置了服务端口、Nacos 注册中心地址、
    以及各微服务的路由规则。
    - 添加 GatewayApplication 启动类，用于启动 Spring Cloud Gateway 服务。

---
+++ hm-gateway/src/main/java/com/hmall/gateway/GatewayApplicaiton.java
@@ -0,0 +1,11 @@
+package com.hmall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplicaiton {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplicaiton.class, args);
+    }
+}
